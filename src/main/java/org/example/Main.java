c627f393fd096184b868fb4e62c2b5347ba86162
qkjm1 <kqk9269@gmail.com>
2025-03-26 17:07:01 +0900

    가로세로 별찍기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,18 @@
 package org.example;
 
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.printf("0 1 1 2 3 5 8 13 21 34");
-
-
-
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for (int j = b; j >0; j--) {  // b에 입력받은 값은 prinln의 줄바꿈으로 b만큼 줄이 만들어진다
+            for(int i =a ;i>0;i--){    // a에 입력받은 값만큼 '*'이 출력되고 print로 인해 줄바꿈없이 출력
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
