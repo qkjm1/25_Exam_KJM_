d14745f77ac8e2e50a1da3dcaac409e3d9eea6fc
qkjm1 <kqk9269@gmail.com>
2025-03-26 17:12:18 +0900

    숫자로 역삼각형 만들기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,12 +7,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int b = sc.nextInt();
-        for (int j = b; j >0; j--) {  // b에 입력받은 값은 prinln의 줄바꿈으로 b만큼 줄이 만들어진다
-            for(int i =a ;i>0;i--){    // a에 입력받은 값만큼 '*'이 출력되고 print로 인해 줄바꿈없이 출력
-                System.out.print("*");
+        int b = 0;
+        for(int i=a;i>0;i--){
+            for(int j=i;a-j>0;j++){
+                System.out.print(" ");
             }
-            System.out.println("");
+            for(int k=a;k-b>0;k--){
+                System.out.print((k-b)+" ");
+            }
+            b++;
+            System.out.println();
         }
     }
 }
